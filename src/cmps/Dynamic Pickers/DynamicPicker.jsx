14453db8cb033cb4365d9e4f018d6c8d1fe9a462
Moshik Alper <moshikalper@gmail.com>
2025-01-19 15:14:32 +0200

    Add DynamicPicker component and it's state management

---
+++ src/cmps/Dynamic Pickers/DynamicPicker.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+import { onTogglePicker } from '../../store/actions/app.actions'
+
+export function DynamicPicker() {
+    const pickerData = useSelector((storeState) => storeState.appModule.pickerData)
+
+    function onClosePicker() {
+        onTogglePicker()
+    }
+
+    if (!pickerData) return null
+    const Cmp = pickerData.cmp
+
+    return (
+        <div className="dynamic-picker-overlay" onClick={onClosePicker}>
+            <div className="dynamic-picker" onClick={(ev) => ev.stopPropagation()}>
+                <section className="picker-content">
+                    {Cmp && <Cmp {...pickerData.props} />}
+                </section>
+            </div>
+        </div>
+    )
+}
